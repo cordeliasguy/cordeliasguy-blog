abcbd375874dc8a31ba9b91fd40e5fc2111e667d
Daniel A. Caballero <danielalejandrocc33@gmail.com>
2022-11-24 12:46:51 +0100

    bug searching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
+import image from '@astrojs/image'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://cordeliasguy-blog.vercel.app/',
+  integrations: [sitemap(), react(), image(), tailwind()]
+})
---
+++ src/components/icons/book-fill.tsx
@@ -0,0 +1,15 @@
+export default function BookFillIcon() {
+  return (
+    <svg
+      className="w-5 inline-block transition-transform group-hover:rotate-[20deg]"
+      viewBox="0 0 512 512"
+      fill="currentColor"
+    >
+      <path
+        d="M160,384h256c17.672,0,32-14.326,32-32V32c0-17.674-14.328-32-32-32h-64v128l-48-32l-48,32V0H128C92.656,0,64,28.652,64,64
+	v320v32c0,52.938,43.063,96,96,96h256c17.672,0,32-14.328,32-32s-14.328-32-32-32H160c-17.648,0-32-14.352-32-32
+	S142.352,384,160,384z"
+      />
+    </svg>
+  )
+}
