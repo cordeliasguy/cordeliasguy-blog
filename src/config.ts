9129419fa1c02803827e254ffb0e506cf194da0d
Daniel A. Caballero <danielalejandrocc33@gmail.com>
2022-12-09 15:12:57 +0100

    custom scrollbar

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'What I use - Daniel A. Caballero'
+export const SITE_TITLE = 'Astro Blog - Daniel A. Caballero'
 export const SITE_DESCRIPTION = 'Just a normal blog'
 export const HOMEPAGE_URL = 'https://cordeliasguy-homepage.vercel.app/'
